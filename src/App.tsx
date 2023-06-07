52b6a6a2c2d045d8c58d805f5609ff1115140b4a
Pavel <zvjxrf@yandex.ru>
2023-06-07 14:35:04 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
+import {SubmitHandler, useForm} from 'react-hook-form'
+import Cookies from 'universal-cookie';
 import './App.css';
 
+
+type Inputs = {
+    field1: number,
+    field2: number,
+}
+
+function isNumber(str: any) {
+    if (typeof str != "string") return false // we only process strings!
+    return !isNaN(Number(str)) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+        !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
+function calculate(v1: number, v2: number){
+    return v1 + v2;
+}
+
 function App() {
+    let coockie = new Cookies()
+
+
+    const {handleSubmit, register, formState: {errors}} = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = formData => {
+        console.log(formData);
+        let result:number = calculate(+formData.field1, +formData.field2)
+        console.log(result);
+        //coockie.set("",result, {})
+    };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrapper">
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <input placeholder="field1"
+                       {...register(
+                           "field1",
+                           {
+                               required: 'This field is required',
+                               validate: {v: (v) => isNumber(v) || "value is not a number"}
+                           })}
+                       className={errors.field1 ? "field1 Error" : undefined}/>
+                {errors.field1 && <span>{errors.field1?.message}</span>}
+            </div>
+            <div>
+                <input placeholder="field2"
+                       {...register(
+                           "field2",
+                           {
+                               required: 'This field is required',
+                               validate: {v: (v) => isNumber(v) || "value is not a number"}
+                           })}
+                       className={errors.field2 ? "passwordError" : undefined}/>
+                {errors.field2 && <span>{errors.field2?.message}</span>}
+            </div>
+            <div>
+                <input type="submit"/>
+            </div>
+        </form>
+
+      <select>
+
+      </select>
     </div>
   );
 }
