ed9bd4171adde54203b99a4c1d74322a9f25b2f6
Pavel <zvjxrf@yandex.ru>
2023-06-09 20:07:37 -0400

    hiding delete button when no checkbox selected

--- src/components/History/History.tsx
+++ src/components/History/History.tsx
@@ -2,10 +2,11 @@ import {SubmitHandler, useForm} from "react-hook-form";
 import {ActionCreatorWithPayload} from "@reduxjs/toolkit";
 import HistoryClasses from "./History.module.css"
 import ButtonsClasses from "./../../common/buttons/Buttons.module.css"
+import React, {useState} from "react";
 
 
 type Inputs = {
-    remove: boolean,
+    readyToRemove: boolean,
 }
 
 interface IHistory {
@@ -15,9 +16,24 @@ interface IHistory {
 
 const History = (props: IHistory) => {
 
+    let [readyToRemove, SetReadyToRemove] = useState<string[]>([])
+    const checkboxToggle = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if(e.target){
+            if(readyToRemove.includes(e.target.value)){
+                SetReadyToRemove(readyToRemove.filter(item => item !== e.target.value))
+                return;
+            }
+            SetReadyToRemove([...readyToRemove, e.target.value]);
+        }
+    }
+    const cleanReadyToRemove = () => {
+        SetReadyToRemove([]);
+    }
+
     const {handleSubmit, register, formState: {errors}} = useForm<Inputs>();
     const onSubmit: SubmitHandler<Inputs> = formData => {
-        props.removeCookie(formData.remove);
+        cleanReadyToRemove();
+        props.removeCookie(formData.readyToRemove);
     };
 
     let elements = props.results.map(item => {
@@ -29,16 +45,17 @@ const History = (props: IHistory) => {
                     <span>Value from field2: {item.valueFromField2}</span><br/>
                     <span>Result: {item.result}</span>
                 </div>
-                <input type="checkbox" value={item.id} {...register("remove",)}/>
+                <input type="checkbox" onInput={checkboxToggle} value={item.id} {...register("readyToRemove",)}/>
             </div>
         )
     });
-
+    console.log(readyToRemove);
     return (
         <div>
             <form onSubmit={handleSubmit(onSubmit)}>
                 {elements}
-                <input type="submit" value="Delete" className={ButtonsClasses.ButtonBasicStyles}/>
+                {elements.length ? undefined : <span className={HistoryClasses.noRecords}>No records</span>}
+                {readyToRemove.length ? <input type="submit" value="Delete" className={ButtonsClasses.ButtonBasicStyles}/> : undefined}
             </form>
         </div>
     )
