1c131c93002fe4b5ea42447c6c68f4f6ca902fe0
Pavel <zvjxrf@yandex.ru>
2023-06-10 13:24:20 -0400

    description text has been added

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -10,7 +10,12 @@ const HomePage = (props: myProps) => {
     return (
         <div className={HomePageClasses.wrapper}>
             <div className={HomePageClasses.description}>
-                Description
+                Hello dear friend!<br/>
+                This simple application can be used for a specific purpose:
+                to calculate the payment for water heating. <br/>
+                The calculator used is only suitable for everyone who lives in <span>"Пик комфорт"</span> apartments. <br/>
+                The application also has a history of your results; however,
+                it keeps your data locally which means you can not see your history using another device or browser.
             </div>
             <div className={HomePageClasses.buttons}>
                 <div className={HomePageClasses.history}>
