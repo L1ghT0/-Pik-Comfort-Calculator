d30b5bdd277d85de33979e2a2b599d761750bb35
Pavel <zvjxrf@yandex.ru>
2023-06-12 13:38:23 -0400

    Fixed an issue with incorrect month indication

--- src/components/CalculateForm/CalculateFormContainer.tsx
+++ src/components/CalculateForm/CalculateFormContainer.tsx
@@ -1,20 +1,24 @@
-import { ActionCreatorWithPayload } from "@reduxjs/toolkit";
-import { connect } from "react-redux";
-import { addCookie } from "../../redux/historyReducer/HistoryReducer";
+import {ActionCreatorWithPayload} from "@reduxjs/toolkit";
+import {connect} from "react-redux";
+import {addCookie} from "../../redux/historyReducer/HistoryReducer";
 import CalculateForm from "./CalculateForm";
 
 function calculate(value1: number, value2: number) {
     return value1 + value2;
 }
 
-function getDate(){
+function getDate() {
     const WeekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let date = new Date();
 
-    return `${WeekDays[date.getDay()]} ${date.getDate() < 10 ? `0${date.getDate()}`
-        : date.getDate()}.${date.getMonth() < 10 ? `0${date.getMonth()}`
-        : date.getMonth()}.${date.getFullYear()} ${date.getHours() < 10 ? `0${date.getHours()}`
-        : date.getHours()}:${date.getMinutes() < 10 ? `0${date.getMinutes()}`
+    return `${WeekDays[date.getDay()]} ${date.getDate() < 10
+        ? `0${date.getDate()}`
+        : date.getDate()}.${date.getMonth() + 1 < 10
+        ? `0${date.getMonth() + 1}`
+        : date.getMonth() + 1}.${date.getFullYear()} ${date.getHours() < 10
+        ? `0${date.getHours()}`
+        : date.getHours()}:${date.getMinutes() < 10
+        ? `0${date.getMinutes()}`
         : date.getMinutes()}`;
 }
 
@@ -25,15 +29,13 @@ interface ICalculateFormContainer {
 
 const CalculateFormContainer = (props: ICalculateFormContainer) => {
 
-    return(
+    return (
         <CalculateForm addCookie={props.addCookie} calculate={calculate} getDate={getDate}/>
     )
 }
 
 const mapStateToProps = () => {
-    return {
-
-    }
+    return {}
 }
 
-export default connect(mapStateToProps,{addCookie})(CalculateFormContainer)
+export default connect(mapStateToProps, {addCookie})(CalculateFormContainer)
